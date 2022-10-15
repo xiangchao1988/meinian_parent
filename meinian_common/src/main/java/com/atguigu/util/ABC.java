75482deb910ea592b145d2c9e9ed2557f4f5a826
xiangchao <48980006+xiangchao1988@users.noreply.github.com>
2022-10-15 20:30:07 +0800

    Update ABC.java

--- meinian_common/src/main/java/com/atguigu/util/ABC.java
+++ meinian_common/src/main/java/com/atguigu/util/ABC.java
@@ -3,5 +3,8 @@ package com.atguigu.util;
 public class ABC {
     public static void ss(){
         System.out.println("dev的打印语句");
+         System.out.println("dev的打印语句");
+         System.out.println("dev的打印语句");
+         System.out.println("dev的打印语句");
     }
 }
